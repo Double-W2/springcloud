dbf0ad6978ca177d1a80c3dc5c38cbbb1d0d0841
Double-W2 <ruihuiforever@gmail.com>
2023-05-31 02:39:43 +0800

    commit  修改ribbon负载均衡的策略

--- springcloud-consumer-dept-80/src/main/java/com/wxr/springcloud/config/BeanConfig.java
+++ springcloud-consumer-dept-80/src/main/java/com/wxr/springcloud/config/BeanConfig.java
@@ -1,5 +1,7 @@
 package com.wxr.springcloud.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +10,16 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class BeanConfig {
     @Bean
-    @LoadBalanced//ribbon LoadBalanced只要加上这个注解，就可以实现负载均衡算法
+    @LoadBalanced//ribbon LoadBalanced只要加上这个注解，就可以实现负载均衡算法，默认实现轮询。
     public RestTemplate getRest(){
         return new RestTemplate();
     }
     /*配置负载均衡实现RestTemplate*/
+
+    /*修改负载均衡的算法，这是针对任何微服务而言（也可以在配置文件中配置单一微服务的规则）。如果要自定义，就要继承AbstractLoadBalancerRule类，实现里面的方法即可。*/
+    @Bean
+    public IRule getRandomRule(){
+        return new RandomRule();
+    }
+
 }
